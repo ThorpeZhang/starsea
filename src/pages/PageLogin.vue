2e460fa97d47ff5fe3d1da6c3882b322fd3def48
ThorpeZhang <1027285564@qq.com>
2019-08-25 08:38:56 +0800

    添加了登录页面

---
+++ src/pages/PageLogin.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <input v-model="usrname" placeholder="请输入用户名"/>
+        <input v-model="password" placeholder="请输入密码" type="password"/>
+        <button onclick="login()">登录</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PageLogin',
+    data: {
+        usrname: '',
+        password: ''
+    },
+    methods: {
+        login: function(){
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function(){
+                if(this.readyState==4&&this.status==200){
+                    alert("登录成功");
+                }
+                else{
+                    alert("登录中");
+                }
+            };
+            xhttp.open("GET","./text.txt?usrname=usrname&password=password",true);
+            xhttp.send();
+        }
+    }
+}
+</script>
