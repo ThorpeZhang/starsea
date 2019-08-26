2ee74b6f79504b7ef71894b3b97fe389cec57fdd
ThorpeZhang <1027285564@qq.com>
2019-08-26 15:55:24 +0800

    修复了代码引用路径的大小写问题

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import pageIndex from '../pages/PageIndex.vue'
-import pageMovie from '../pages/PageMovie.vue'
-import pageBook from '../pages/PageBook.vue'
-import pageGroup from '../pages/PageGroup.vue'
+import pageIndex from '@/pages/PageIndex.vue'
+import pageMovie from '@/pages/PageMovie.vue'
+import pageBook from '@/pages/PageBook.vue'
+import pageGroup from '@/pages/PageGroup.vue'
 
-import movieIndex from '../pages/movieIndex.vue'
-import movieDetail from '../pages/movieDetail.vue'
-import pageLogin from '../pages/PageLogin.vue'
-import pageRegister from '../pages/PageRegister.vue'
+import movieIndex from '@/pages/movieIndex.vue'
+import movieDetail from '@/pages/movieDetail.vue'
+import pageLogin from '@/pages/PageLogin.vue'
+import pageRegister from '@/pages/PageRegister.vue'
 
 Vue.use(Router)
 
